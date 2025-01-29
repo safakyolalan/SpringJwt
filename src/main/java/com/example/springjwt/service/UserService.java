927ee64f4a52a9d519c09ee61d2031ddb379488f
safakyolalan <safakyolalan752@gmail.com>
2025-01-29 23:08:21 +0300

    yeni dosyalar yüklendi

--- src/main/java/com/example/springjwt/service/UserService.java
+++ src/main/java/com/example/springjwt/service/UserService.java
@@ -1,12 +1,9 @@
 package com.example.springjwt.service;
-<<<<<<< HEAD
 
 import com.example.springjwt.domain.Role;
 import com.example.springjwt.domain.User;
 
 import java.util.List;
-=======
->>>>>>> 70eddb99d489a91d9f0fbb52be4cc3f7554fbe46
 
 public interface UserService {
     User saveUser(User user);
